a3a9d1ec46bc038d6f98cb783597ee1f2705eada
barnettZQG <zengqg@goodrain.com>
2021-02-05 08:14:38 +0800

    get promql by env

--- main.go
+++ main.go
@@ -4,23 +4,24 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
 )
 
 func main() {
-	promql := flag.String("promql", "", "The prometheus query language to be ensure the service_id label")
+	promql := os.Getenv("PROMQL")
 	// component_id and service_id are the same. service_id is the old name, and component_id is the new name
 	serviceID := flag.String("component_id", "", "The value of label service_id")
 
 	flag.Parse()
 
-	if *promql == "" {
+	if promql == "" {
 		log.Fatal("The arg 'promql' is required")
 	}
 	if *serviceID == "" {
 		log.Fatal("The arg 'component_id' is required")
 	}
 
-	newPromQL, err := ensureServiceID(*promql, *serviceID)
+	newPromQL, err := ensureServiceID(promql, *serviceID)
 	if err != nil {
 		log.Fatal(err)
 	}
